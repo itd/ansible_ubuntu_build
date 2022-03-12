f37cfab78cf9121b5d21a50f3d15b4462483f305
Kurt Bendl <kurt@tool.net>
2022-03-12 12:00:39 -0500

    Update makeall.sh

--- makeall.sh
+++ makeall.sh
@@ -5,6 +5,8 @@ gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_modifier'
 # go on then...
 python3 -m venv venv
 source venv/bin/activate
+pip install -U pip
+pip install wheel
 pip install -Ur requirements.txt
 ansible-galaxy install -r requirements.yml
-ansible-playbook ./build_laptop.yml -v -u $USER -b
+ansible-playbook ./build_laptop.yml -v -u $USER -C -b
